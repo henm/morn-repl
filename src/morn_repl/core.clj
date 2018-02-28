54c5be297465233f327f45cdab5d13fc66890d00
henm <henm@users.noreply.github.com>
2018-02-28 21:03:06 +0100

    Minimal repl
    Just for the fun of it.

--- src/morn_repl/core.clj
+++ src/morn_repl/core.clj
@@ -3,9 +3,28 @@
   (:require [clojure.string :as string]
             [morn-repl.parser :as parser]))
 
+(defn exit "Exit the repl" []
+  (println)
+  (println "Bye!")
+  (System/exit 0))
+
+(defn eof? "Check if EOF was read" [line] (nil? line))
+
+(defn empty-line? "Check if empty line was read" [line] (empty? line))
+
+(defn repl-loop
+  "Read line by line"
+  []
+  (print "> ")
+  (flush)
+  (let [line (read-line)]
+    (cond (eof? line) (exit)
+          (empty-line? line) ()
+          :else (println (parser/parse-rule line))))
+  (recur))
+
 (defn -main
-  "I don't do a whole lot ... yet."
+  "Start morn-repl!"
   []
   (println "Welcome to morn-repl!")
-  (let [line (read)]
-    (println line)))
+  (repl-loop))
