207947122c6adf33c83d31bb6ae00780c15220e1
henm <henm@users.noreply.github.com>
2018-02-28 21:45:31 +0100

    Added dependency to local morn

--- project.clj
+++ project.clj
@@ -3,7 +3,9 @@
   :url "https://github.com/henm/morn-repl"
   :license {:name "Apache License, Version 2.0"
             :url "http://www.apache.org/licenses/LICENSE-2.0"}
-  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [de.henm/morn.core "0.1-SNAPSHOT"]]
+  :repositories { "local" "~/.m2/repository/"}
   :main ^:skip-aot morn-repl.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
